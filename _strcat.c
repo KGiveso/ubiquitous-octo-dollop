d111a218c8b49799778ea1983174a4e62a5ac225
Tshepiso Given <75566615+KGiveso@users.noreply.github.com>
2023-05-23 12:50:05 +0200

    Update _strcat.c

--- _strcat.c
+++ _strcat.c
@@ -12,10 +12,10 @@
 char *_strcat(char *dest, char *src)
 {
 
-	int index = 0, dest_len = 0;
+	int i = 0, string = 0;
 
-	while (dest[index++])
-		dest_len++;
+	while (dest[i++])
+		++;
 
 	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
